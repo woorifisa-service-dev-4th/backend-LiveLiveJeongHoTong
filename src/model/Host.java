19e8f2c4c770d65aa4a8a14fc53ad64b22ffd50c
Hanbee Lee <bee1162@naver.com>
2025-02-12 18:01:27 +0900

    fix: 생성자 변경

--- src/model/Host.java
+++ src/model/Host.java
@@ -1,15 +1,10 @@
 package model;
+public class Host extends Person{
 
-public class Host  extends Person{
     private int id;
-   private String email;
-   private String password;
+    private String email;
+    private String password;
 
-   public Host(int id, String email, String password) {
-       this.id = id;
-       this.email = email;
-       this.password = password;
-   }
     public Host(String email) {
         this.email = email;
     }
