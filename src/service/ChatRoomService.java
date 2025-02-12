781e6aaad947286c0c8d4cea6061c5af471f1420
sengjun0624 <sengjun0624@naver.com>
2025-02-12 12:44:29 +0900

    feat: 1,2 외에 다른 옵션 선택 금지 추가

--- src/service/ChatRoomService.java
+++ src/service/ChatRoomService.java
@@ -11,7 +11,7 @@ public class ChatRoomService {
 	private final ChatRoomRepository chatRoomRepository = new ChatRoomRepository();
 
 	public void createChatRoom(String roomName) throws IOException {
-		if(!validateRoomName(roomName)){
+		if(!isRoomNameValid(roomName)){
 			throw new IOException("1글자 이상 입력해주세요");
 		}
 		ChatRoom chatRoom = new ChatRoom(roomName);
---
+++ src/validator/InputNameValidator.java
@@ -0,0 +1,14 @@
+package validator;
+
+import java.io.IOException;
+
+import model.ChatRoom;
+
+public class InputNameValidator {
+	public static boolean isOptionValid(String options) throws IOException {
+		if (!options.equals("1") && !options.equals("2")) {
+			throw new IOException("잘못된 입력입니다. 1 또는 2를 ");
+		}
+		return true;
+	}
+}
