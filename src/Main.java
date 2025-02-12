a50afb93e9c1d6a687a2c6e0c83534a99d43d68d
Hanbee Lee <bee1162@naver.com>
2025-02-12 12:36:29 +0900

    feat:chat 서비스 클래스 내부 동작 구현

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import service.GuestChatService;
+
 public class Main {
 	public static void main(String[] args) {
+		GuestChatService service = new GuestChatService();
+
+		service.enter(100);
+
 		System.out.println("Hello world!");
 	}
 }
