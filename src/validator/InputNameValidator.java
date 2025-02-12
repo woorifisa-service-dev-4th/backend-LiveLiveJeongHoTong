9069674f7c1cd146db5ea7a54c24909f1cb5c43b
sengjun0624 <sengjun0624@naver.com>
2025-02-12 15:14:43 +0900

    fix: NPE 방지 코드 추가

--- src/validator/InputNameValidator.java
+++ src/validator/InputNameValidator.java
@@ -1,14 +1,17 @@
 package validator;
 
 import java.io.IOException;
+import java.util.Objects;
 
 import model.ChatRoom;
 
 public class InputNameValidator {
 	public static boolean isOptionValid(String options) throws IOException {
+		options = Objects.requireNonNull(options, "⚠ 입력 오류: 잘못된 입력입니다.\n"); // NPE 발생
 		if (!options.equals("1") && !options.equals("2")) {
 			throw new IOException("⚠ 입력 오류: 잘못된 입력입니다. 1 또는 2를 입력하세요.\n");
 		}
 		return true;
 	}
+
 }
--- src/validator/RoomNameValidator.java
+++ src/validator/RoomNameValidator.java
@@ -1,10 +1,13 @@
 package validator;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class RoomNameValidator {
 	public static void isRoomNameValid(String roomName) throws IOException{
+		roomName = Objects.requireNonNull(roomName, "⚠ 입력 오류: 잘못된 입력입니다.\n"); // NPE 발생
 		if(roomName.isEmpty())
 			throw new IOException("⚠ 입력 오류: 잘못된 입력입니다. 1글자 이상 입력하세요.\n");
 	}
+
 }
